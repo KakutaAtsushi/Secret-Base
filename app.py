b9e0f4b1f88ee6916e159f4351e0c29b818b73aa
Kakip1919 <invest9me@yahoo.co.jp>
2021-07-17 13:49:09 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
+from flask_socketio import SocketIO, emit
+from flask_sockets import Sockets
+# git add .
+# git commit -m"1st commit"
+# git push heroku master
+# 更新手順
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hogehugahage'
 
 
 @app.route('/')
@@ -9,11 +17,19 @@ def Home_index():
     return render_template("index.html", title="flask test", name=name)
 
 
-
 @app.route("/reference")
 def My_reference():
-    return render_template("reference.html", title="Reference_mypage",)
+    return render_template("reference.html", title="Reference_mypage", )
 
+
+@app.route("/Lambda")
+def Lambda_Action():
+    return render_template("Lambda.html", title="Lambda_notify")
+
+
+@app.route("/burst_demo")
+def bust_index():
+    return render_template("bustabit_demo.html", title="bustabit_demo")
 
 
 if __name__ == '__main__':
